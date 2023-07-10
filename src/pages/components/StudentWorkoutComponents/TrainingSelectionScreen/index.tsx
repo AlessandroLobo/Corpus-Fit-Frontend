2f87d23d860a1045cf9e3c79f4eb39594cf5eb9a
Alessandro Lobo <alessandro.lobo@hotmail.com>
2023-07-10 17:03:17 -0300

    Starting create TrainingSelectionScreen

--- src/pages/components/StudentWorkoutComponents/TrainingSelectionScreen/index.tsx
+++ src/pages/components/StudentWorkoutComponents/TrainingSelectionScreen/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Container, Form } from './styles'
+import React, { useState } from 'react'
+import { Container, Form, Button, ButtonContainer } from './styles'
 
 export default function TrainingSelectionScreen() {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const slides = ['Slide 1', 'Slide 2', 'Slide 3'] // Substitua pelo conteúdo dos seus slides
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === slides.length - 1 ? 0 : prevSlide + 1,
+    )
+  }
+
+  const handlePreviousSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? slides.length - 1 : prevSlide - 1,
+    )
+  }
+
   return (
     <Container>
-      <Form>TrainingSelectionScreen</Form>
+      <ButtonContainer>
+        <Button onClick={handlePreviousSlide}>Previous</Button>
+        <Button onClick={handleNextSlide}>Next</Button>
+      </ButtonContainer>
+      <Form>{slides[currentSlide]}</Form>
     </Container>
   )
 }
